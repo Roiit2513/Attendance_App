4e5f25d06ce23e81d7eb25c47368fcb42f5b235f
Rohit <patilrohit2513@gmail.com>
2023-06-13 12:23:34 +0530

    initial setup

---
+++ my-app/app/account/register/page.js
@@ -0,0 +1,12 @@
+
+export default function Register() {
+    return (
+      <div className="flex flex-col">
+        <div id="title" className="flex items-center justify-center bg-emerald-300">
+          <p className="text-center text-4xl my-12 font-bold">Register page</p>
+        </div>
+        
+      </div>
+    )
+  }
+  
