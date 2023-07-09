7fa8c804b895a489943b3f2e1da7c3041e15660a
Rohit <patilrohit2513@gmail.com>
2023-07-09 23:01:22 +0530

    minor changes

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <p id='title' className='text-2xl text-center'>College Attendance Record</p>
       </div>
       <div className='m-4'>
-        <p className='text-center'>All in one App to maintain and keep tabs on your school / college attendeance and never miss out on your grades.</p>
+        <p className='text-center'>All in one App to maintain and keep tabs on your school / college attendance and never miss out on your grades.</p>
       </div>
     </>
   )
